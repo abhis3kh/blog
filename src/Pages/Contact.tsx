4864a66d3eb8e5fc5646a335e7cc04886d8c4ef7
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2024-03-12 01:48:23 +0530

    Contact Me page added with proper functionality

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import './Contact.css';
+// ! TODO : Add error handling for invalid inputs
 const Contact = () => {
+  const handleContactMe = (e) => {
+    e.preventDefault();
+    let message = e.target['message'].value;
+    let name = e.target['name'].value;
+    // Trim the value
+    message = message.trim();
+    name = name.trim();
+    // Create a mailto link
+    const mailtoLink = `mailto:abhisekhmukherjee101@gmail.com?subject=Enquiry from Website%20Form&body=${encodeURIComponent(
+      `${message} \n\n\n\n\n Sender Name : ${name}`
+    )}`;
+
+    // Open the default mail app
+    window.location.href = mailtoLink;
+  };
+  const clearContent = () => {
+    document.getElementById('name').value = '';
+    document.getElementById('message').value = '';
+  };
+
   return (
-    <div className='App'>
+    <div>
       <header>
         <Navbar />
       </header>
-      <div>Contact</div>
+      <div className='contact-info'>
+        <h1>Contact ME</h1>
+        <form onSubmit={handleContactMe} method='POST'>
+          <label htmlFor='name'>Provide your name</label>
+          <input type='text' id='name' placeholder='Please provide your name' />
+          <label htmlFor='message'>Enter your Message </label>
+          <textarea
+            name='message'
+            id='message'
+            cols='60'
+            rows='10'
+            placeholder='Please enter your message here'
+          ></textarea>
+          <button type='submit'>Send Message</button>
+          <br></br>
+        </form>
+        <button type='button' onClick={clearContent}>
+          Clear Content
+        </button>
+      </div>
     </div>
   );
 };
